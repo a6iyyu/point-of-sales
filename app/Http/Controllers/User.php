a8f05bd8d5b9782b4ef9f96ea978ca1d8912c111
a6iyyu <mizukinako7@gmail.com>
2025-03-04 09:33:37 +0700

    Selesai mengerjakan Retrieving or Creating Models.

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User as UserModel;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class User extends Controller
 {
     public function index(): View
     {
-        return view('pengguna', ['data' => UserModel::where('level_id', 2)->count()]);
+        $user = UserModel::firstOrNew([
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ])->save();
+
+        return view('pengguna', ['data' => $user]);
     }
 }
